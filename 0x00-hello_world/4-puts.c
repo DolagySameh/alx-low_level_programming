2bc7458fcb891eaed1b69603e55a26520b644002
DolagySameh <dolagysameh20@gmail.com>
2023-08-10 11:53:03 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
- * main funciton 
- * entry point
+ * mai prints sentecs , 
+ * followed by new line,
+ * return 0.
  * */
 int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
