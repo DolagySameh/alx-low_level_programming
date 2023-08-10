f6fd773e87ea6baa43a1a528df3e6a74f90e8137
DolagySameh <dolagysameh20@gmail.com>
2023-08-10 12:04:27 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
-* function main prints exactly with proper grammar, but the outcome is a piece of art,
+* function main prints Programming is like building a multilingual puzzle,
 * followed by new line,
 * return 0 .
 */
