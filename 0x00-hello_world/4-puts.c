4799980ad4ae0c44e52994279d3736142f411dc4
DolagySameh <dolagysameh20@gmail.com>
2023-08-10 11:58:02 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  * mai prints sentecs , 
  * followed by new line,
  * return 0.
- * */
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	return (0);
 }
