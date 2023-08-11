52a58f380293f7f75c1cf72c9a4379d45f5ce1f4
DolagySameh <dolagysameh20@gmail.com>
2023-08-11 11:46:28 -0700

    DSDS

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+* main - combination of three digit
+* Description:by putchar function
+* Return:0
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i < 57)
+	{
+		int j = 48;
+
+		while (j < 57)
+		{
+			int k = 48;
+
+			while (k < 57)
+			{
+				if (i != j && j != k && i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i + j + k != 165)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			k++;
+			}
+		j++;
+		}
+	i++;
+	}
+	putchar('\n');
+	return (0);
+}
