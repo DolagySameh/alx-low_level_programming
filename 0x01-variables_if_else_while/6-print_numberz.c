31b5e0f2433f4f50ea2c328234a70c9f36bf99ef
DolagySameh <dolagysameh20@gmail.com>
2023-08-11 09:12:07 -0700

    DSD

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - print digit number
+* Description: wusing printf
+* Return:0
+*/
+int main(void)
+{
+	int a = 48;
+
+	while (a < 58)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
