8567dfee59237591532039f91899e7ba04e2ecb7
DolagySameh <dolagysameh20@gmail.com>
2023-08-11 09:31:08 -0700

    DSD

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,4 +1,23 @@
 #include <stdio.h>
 /**
-* main -  
+* main - hexadecimal
+* Description:using put char
+* Return:0
 */
+int main(void)
+{
+	int n = 48, n2 = 'A';
+
+	while (n < 58)
+	{
+		putchar(n);
+		n++;
+	}
+	while (n2 <= 'F')
+	{
+		putchar(n2);
+		n2++;
+	}
+	putchar('\n');
+	return (0);
+}
