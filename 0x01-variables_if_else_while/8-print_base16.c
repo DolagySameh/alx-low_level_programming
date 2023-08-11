b87d069918370d502e9587b2a4eec85c729b1e2d
DolagySameh <dolagysameh20@gmail.com>
2023-08-11 09:32:06 -0700

    DSD

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,14 +6,14 @@
 */
 int main(void)
 {
-	int n = 48, n2 = 'A';
+	int n = 48, n2 = 'a';
 
 	while (n < 58)
 	{
 		putchar(n);
 		n++;
 	}
-	while (n2 <= 'F')
+	while (n2 <= 'f')
 	{
 		putchar(n2);
 		n2++;
