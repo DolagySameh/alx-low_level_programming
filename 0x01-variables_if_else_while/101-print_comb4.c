5e31a8cab324e36ec7a1bbe74105ab7643dee990
DolagySameh <dolagysameh20@gmail.com>
2023-08-11 11:49:01 -0700

    DSDS

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,22 +8,22 @@ int main(void)
 {
 	int i = 48;
 
-	while (i < 57)
+	while (i <= 57)
 	{
 		int j = 48;
 
-		while (j < 57)
+		while (j <= 57)
 		{
 			int k = 48;
 
-			while (k < 57)
+			while (k <= 57)
 			{
 				if (i != j && j != k && i < j && j < k)
 				{
 					putchar(i);
 					putchar(j);
 					putchar(k);
-					if (i + j + k != 165)
+					if (i + j + k != 168)
 					{
 						putchar(',');
 						putchar(' ');
