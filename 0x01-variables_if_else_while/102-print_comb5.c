e685b313333600de09e5aab0efb9c7ab89e745e0
DolagySameh <dolagysameh20@gmail.com>
2023-08-11 12:26:29 -0700

    FFR

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+/**
+* main - four compination
+* Description:unsing putchar
+* Return:0
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		int j = 48;
+
+		while (j <= 57)
+		{
+			int k = 48;
+
+			while (k <= 57)
+			{
+				int l = 48;
+
+				while (l <= 57)
+				{
+					if (i <= k && j <= l && i + j != k + l)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+						if (i + j + k + l != 227)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				l++;
+				}
+			k++;
+			}
+		j++;
+		}
+	i++;
+	}
+	putchar('\n');
+	return (0);
+}
