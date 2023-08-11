f69f7b8455aeebd8e2651b10519f038b0a9c0565
DolagySameh <dolagysameh20@gmail.com>
2023-08-11 09:18:47 -0700

    DSD

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - print lowercase in reverse
+* Description:using puchar
+* Return:0
+*/
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch > 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
+
