35e4a49c00f2594c14bd9241e7347738d15ca0bc
DolagySameh <dolagysameh20@gmail.com>
2023-08-15 16:25:01 -0700

    hello c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,25 +14,28 @@ void jack_bauer(void)
 
 		while (b <= 57)
 		{
-			int c = 48;
-
-			while (c <= 53)
+			if ((a == 2 && b == 4) || (a == 2 && b == 5) || (a == 2 && b == 6) || (a == 2 && b == 7) || (a == 2 && b == 8) || (a == 2 && b == 9))
 			{
-				int d = 48;
+				int c = 48;
 
-				while (d <= 57)
+				while (c <= 53)
 				{
-					_putchar(a);
-					_putchar(b);
-					_putchar(':');
-					_putchar(c);
-					_putchar(d);
-					_putchar('\n');
-					d++;
+					int d = 48;
+
+					while (d <= 57)
+					{
+						_putchar(a);
+						_putchar(b);
+						_putchar(':');
+						_putchar(c);
+						_putchar(d);
+						_putchar('\n');
+						d++;
+					}
+				c++;
 				}
-			c++;
+			b++;
 			}
-		b++;
 		}
 	a++;
 	}
