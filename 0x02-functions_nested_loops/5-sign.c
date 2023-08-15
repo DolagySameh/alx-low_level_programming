cd4e57c93dcdb6c8e6444a2ad30aab171df935f1
DolagySameh <dolagysameh20@gmail.com>
2023-08-15 14:41:36 -0700

    hello c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_sign - print
+* Description:print
+* @n:first arg
+* Return:2 or 0
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
