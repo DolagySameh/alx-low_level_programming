c533313ca2354110cd2f7e5f348cd54441c290b9
DolagySameh <dolagysameh20@gmail.com>
2023-08-15 14:56:17 -0700

    hello c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _abs - print
+* Description:print
+* @a:first age
+* Return:intvalue
+*/
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		a = (-1) * a;
+		return (a);
+	}
+	else
+	{
+		return (a);
+	}
+}
