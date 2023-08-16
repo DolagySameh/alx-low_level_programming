65f929abefa322fce94fc9f82701c4176a8c5a0e
DolagySameh <dolagysameh20@gmail.com>
2023-08-15 19:36:10 -0700

    hello c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_times_table - print
+* Description:print
+* @n:first arg
+*/
+void print_times_table(int n)
+{
+	int i;
+
+	if (n < 15 || n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			int j;
+
+			for (j = 0; j <= n; j++)
+			{
+				int z = i * j;
+
+				if (z < 9)
+					_putchar(z + '0');
+				else
+				{
+					_putchar((z / 10) + 48);
+					_putchar((z % 10) + 48);
+				}
+				_putchar(',');
+				_putchar(' ');
+			}
+		_putchar('\n');
+			
+		}
+
+	}
+}
