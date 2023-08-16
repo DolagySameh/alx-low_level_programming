b6783eabe1088fdef3df61f22c8447d888af3b86
DolagySameh <dolagysameh20@gmail.com>
2023-08-15 19:53:53 -0700

    hello c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - print fib
+* Description:fibonacci number
+* Return:0
+*/
+int main(void)
+{
+	int i, a = 1, b = 2;
+
+	for (i = 0; i <= 50; i++)
+	{
+		printf("%d, ", a);
+		int result;
+
+		result = a + b;
+		a = b;
+		b = result;
+	}
+	printf("\n");
+	return 0;
+}
+
