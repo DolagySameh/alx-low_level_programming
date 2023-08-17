dff6233a046b05039ce263245289f563456d3f1b
DolagySameh <dolagysameh20@gmail.com>
2023-08-17 08:46:28 -0700

    check is upper

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_numbers - print
+* Description:print
+*/
+void print_numbers(void)
+{
+	int b = 48;
+
+	while (b <= 57)
+	{
+		_putchar(b);
+		b++;
+	}
+	_putchar('\n');
+}
