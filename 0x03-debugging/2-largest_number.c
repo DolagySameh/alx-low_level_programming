4b5eea694acac1b6716eef775d3220c6e27b725d
DolagySameh <dolagysameh20@gmail.com>
2023-08-17 06:50:09 -0700

    debugging_comment_error_line

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,10 +12,9 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	a = a * (-1);
-	if (a > b && b > c)
+	if (a > b && a > c)
 		largest = a;
-	else if (b > a && a > c)
+	else if (b > a && b > c)
 		largest = b;
 	else
 		largest = c;
