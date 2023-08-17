e70a1b64be23d36472249791db7fcb4ab6c36703
DolagySameh <dolagysameh20@gmail.com>
2023-08-17 08:36:40 -0700

    check is upper

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* mul - multiply
+* Description:multiply
+* @a:first arg
+* @b:second arg
+* Return:integer
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
