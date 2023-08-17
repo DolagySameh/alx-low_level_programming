460e329d6161cdfb51b2381b6dde1a8cad9513dc
DolagySameh <dolagysameh20@gmail.com>
2023-08-17 10:51:06 -0700

    check is upper

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - print
+* Description:print
+* Return:0
+*/
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0)
+		{
+			printf(" Fizz ");
+		}
+		else if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf(" FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+		printf(" ");
+		i++;
+	}
+	return (0);
+}
