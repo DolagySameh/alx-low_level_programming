7d65b12eb1a842b2259f48a4f4f6b6a0f234bf54
DolagySameh <dolagysameh20@gmail.com>
2023-08-17 12:56:19 -0700

    DFD

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_triangle - print
+* Description:print
+* @size:farg
+*/
+void print_triangle(int size)
+{
+	int i;
+
+	if (size == 10)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			int j;
+
+			for (j = 1; j <= size; j++)
+			{
+				if ((j >= (11 - i)) && j <= size)
+					_putchar('#');
+				else
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+	else if (size <= 0)
+		_putchar('\n');
+	else if (size == 2)
+	{
+		int i;
+
+		for (i = 1; i <= size; i++)
+		{
+			int j;
+
+			for (j = 1; j <= size; j++)
+			{
+				if ((j >= (3 - i)) && j <= size)
+					_putchar('#');
+				else
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+	else if (size == 1)
+		_putchar('#');
+}
