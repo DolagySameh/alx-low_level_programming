139abefae23c551ca8110f770dbe1147c1a75c71
DolagySameh <dolagysameh20@gmail.com>
2023-08-22 06:34:00 -0700

    print even

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* puts_half - print
+* Description:print
+* @str:arg
+*/
+void puts_half(char *str)
+{
+	int i = 0, j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	for (j = (i / 2); str[j] != '\0'; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
+
