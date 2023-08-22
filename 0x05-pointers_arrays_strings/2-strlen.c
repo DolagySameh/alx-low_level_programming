cc4a243352e976dfd49edb15f76f63deaee60526
DolagySameh <dolagysameh20@gmail.com>
2023-08-22 03:44:38 -0700

    hello pointer

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+* _strlen - print
+* Description:print
+* @s:first arg
+* Return:integer 0 or 1
+*/
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
