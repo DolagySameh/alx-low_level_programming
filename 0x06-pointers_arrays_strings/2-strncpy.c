af7dc4b30e050515592745d1997116ede718f82a
DolagySameh <dolagysameh20@gmail.com>
2023-08-24 13:28:46 -0700

    strcpy_function

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _strncpy - copy
+* Description:copy
+* @dest:first arg
+* @src:second arg
+* @n:third arg
+* Return:return pointer
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i >= n && i < 98 - 1)
+	{
+		dest[i] = '*';
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
