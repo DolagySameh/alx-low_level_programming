e3711a0afb1c1a3546ab62234952470975eae326
DolagySameh <dolagysameh20@gmail.com>
2023-08-28 16:33:23 -0700

    sixth problem

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
-#include <string.h>
 #include "main.h"
 /**
-* _strstr -  locates a substring.
-* Description: locates a substring.
+* _strstr - locates a substring.
+* Description:locates a substring.
 * @haystack:first arg
 * @needle:second arg
 * Return:pointer
 */
 char *_strstr(char *haystack, char *needle)
 {
-	int k = 0, i = 0;
+	int k = 0, i = 0, j;
 
 	while (needle[k] != '\0')
-	{
 		k++;
-	}
 	for (; needle[i] != '\0'; i++)
 	{
-		int j = 0;
-
+		j = 0;
 		for (; haystack[j] != '\0'; j++)
 		{
 			while (needle[i] == haystack[j] && needle[i] != '\0')
@@ -29,9 +25,13 @@ char *_strstr(char *haystack, char *needle)
 			}
 			if (needle[i - 1] == haystack[j - 1] && needle[i] == '\0')
 				return (&haystack[j - k]);
+			if (needle[i - 1] == haystack[j - 1] && needle[i] != '\0' && haystack[j] == '\0')
+				return (0);
 			i = 0;
 			continue;
 		}
+		if (needle[0] != haystack[j - 1] && haystack[j] == '\0' && needle[i] != '\0')
+			return (0);
 	}
 	return (0);
 }
