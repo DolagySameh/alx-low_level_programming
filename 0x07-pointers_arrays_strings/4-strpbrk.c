a3eaa4224f6cc8caeb62ff72b3db52e9fae44c98
DolagySameh <dolagysameh20@gmail.com>
2023-08-28 10:18:57 -0700

    commit fifth problem

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+* _strpbrk -  searches a string for any of a set of bytes.
+* Description: searches a string for any of a set of bytes.
+* @s:first arg
+* @accept:second arg
+* Return:pointer
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+			else
+			{
+				j++;
+				continue;
+			}
+		}
+		if (s[i] == accept[j])
+			break;
+		if (s[i] == accept[j])
+			return (NULL);
+		i++;
+		}
+	return (s + i);
+}
