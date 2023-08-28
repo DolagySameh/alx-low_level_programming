4aa637b9070faf5fb6163a2f82bb39000169151d
DolagySameh <dolagysameh20@gmail.com>
2023-08-28 12:00:29 -0700

    commit sixth problem

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+* _strstr -  locates a substring.
+* Description: locates a substring.
+* @haystack:first arg
+* @needle:second arg
+* Return:pointer
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j;
+
+	while (needle[i] != '\0')
+	{
+		j = 0;
+		while (haystack[j] != '\0')
+		{
+			if (haystack[j] == needle[i])
+			{
+				return (haystack + j);
+			}
+			j++;
+			continue;
+		}
+		if (needle[i] == haystack[j])
+			break;
+		i++;
+	}
+	return (0);
+}
