af654a413a4b21236b331d48bfd7133bbce7b8bd
DolagySameh <dolagysameh20@gmail.com>
2023-08-28 19:05:20 -0700

    eight commit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* set_string - sets the value
+* Description:sets the value
+* @s:first arg
+* @to:second arg
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
