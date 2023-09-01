d5a3969ee1076acc17d93b58c55cca0f6fee6061
DolagySameh <dolagysameh20@gmail.com>
2023-09-01 12:49:27 -0700

    SECOND task arguments main function

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - prints the number of arguments passed into it
+* Description:prints the number of arguments passed into it
+* @argc:firs arg
+* @argv:second arg
+* Return:0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv;
+	return (0);
+}
