4e1b3b76f56d60576cef580fe7650054de0f4d54
DolagySameh <dolagysameh20@gmail.com>
2023-09-05 14:33:36 -0700

    Second commit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,7 +11,7 @@ char *_strdup(char *str)
 {
 	 int i = 0;
 
-	char *ptr = (char *)malloc(strlen(str) + 1);
+	char *ptr = (char *)malloc(strlen(str));
 
 	if (ptr == NULL || str == NULL)
 		return (NULL);
