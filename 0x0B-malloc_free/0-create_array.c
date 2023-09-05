866a10518a15adeb56a50bd0cb28ff5a4ddadcbe
DolagySameh <dolagysameh20@gmail.com>
2023-09-05 07:43:04 -0700

    FIRST commit OF malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 /**
 * create_array - create array
@@ -9,19 +9,16 @@
 */
 char *create_array(unsigned int size, char c)
 {
-	char *ptr = (char*)malloc(size + 1);
+	char *ptr = (char *)malloc(size + 1);
 
 	if (ptr == NULL || size == 0)
 		return (NULL);
-	else
-	{
-		int i;
+	unsigned int i;
 
-		for (i = 0; i < size; i++)
-		{
-			ptr[i] = c;
-		}
-		ptr[i] = '\0';
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
 	}
+	ptr[i] = '\0';
 	return (ptr);
 }
