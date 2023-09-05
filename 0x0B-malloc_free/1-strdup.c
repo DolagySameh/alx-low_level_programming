e711a703c7afde28c740ebb1ee18646a61677eab
DolagySameh <dolagysameh20@gmail.com>
2023-09-05 08:29:35 -0700

    FIRST commit OF malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* _strdup - copy of the string
+* Description:copy of the string
+* @str:first arg
+* Return:char
+*/
+char *_strdup(char *str)
+{
+	int i = 0;
+
+	char *ptr = (char *)malloc(strlen(str) + 1);
+
+	if (ptr == NULL || str == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
