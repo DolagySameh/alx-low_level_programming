9439e60bf5606b42e0009c05f02ffd9cfeb25a14
DolagySameh <dolagysameh20@gmail.com>
2023-09-07 03:25:41 -0700

    COMMIT FIFTH task

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* free_grid - frees a 2 dimensional grid
+* Description:frees a 2 dimensional grid
+* @grid:first arg
+* @height:second arg
+*/
+void free_grid(int **grid, int height)
+{
+	int i = height - 1;
+	while(i--)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
