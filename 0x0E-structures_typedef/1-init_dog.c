8b721b6bdee0ffff8db3cbeb22c3c9fcfbfe3d81
DolagySameh <dolagysameh20@gmail.com>
2023-09-11 16:57:44 -0700

    SECOND commit struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+* init_dog -  initialize a variable of type struct dog
+* Description: initialize a variable of type struct dog
+* @d:farg
+* @name:sarg
+* @age:targ
+* @owner:foarg
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;	
+}
