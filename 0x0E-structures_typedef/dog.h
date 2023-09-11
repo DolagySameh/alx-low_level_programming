383f2ac88cc2e58b39e118f15172500b798ba029
DolagySameh <dolagysameh20@gmail.com>
2023-09-11 16:41:04 -0700

    FIRST commit struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+* struct dog - Define a new type struct dog
+* Description:Define a new type struct dog
+* @name:first member
+* @age:second member
+* @owner:third memeber
+*/
 struct dog
 {
 	char *name;
