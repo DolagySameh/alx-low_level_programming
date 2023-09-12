2f8cfb498e0edfb8ac77f0b45f6233fef37cffaa
DolagySameh <dolagysameh20@gmail.com>
2023-09-11 17:30:55 -0700

    THIRD commit struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,14 +9,8 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (d->name == NULL)
-			printf("Name: (nil)");
-		printf("Name: %s\n", d->name);
-		if (d->age == 0.0)
-			printf("Age: (nil)");
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
 		printf("Age: %f\n", d->age);
-		if (d->owner == NULL)
-			printf("Owner: (nil)");
-		printf("Owner: %s\n", d->owner);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
