e92b7cf2a85e486e98ed2fdf5cc3b005a11ba35b
DolagySameh <dolagysameh20@gmail.com>
2023-09-11 18:04:11 -0700

    FIFTH commit struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+/**
+* new_dog -  creates a new dog.
+* Description: creates a new dog.
+* @name:first arg
+* @age:targ
+* @owner:second arg
+* Return:pointer
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t d;
+	dog_t *p;
+
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+	p = &d;
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
+}
