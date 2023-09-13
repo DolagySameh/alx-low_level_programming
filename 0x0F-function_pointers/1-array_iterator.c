a9199013c0fdaf99da1812539f26cd08603d2c17
DolagySameh <dolagysameh20@gmail.com>
2023-09-13 13:01:46 -0700

    SECOND commit Task

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+* array_iterator - executes a function
+* Description:executes a function given as a parameter
+* @array:first arg
+* @size:second arg
+* @action:third arg
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	if (action && size)
+	{
+		while (i < size)
+		{
+			(*action)(array[i]);
+			i++;
+		}
+	}
+}
