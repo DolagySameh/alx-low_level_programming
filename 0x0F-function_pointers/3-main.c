16ca9acac50206c1acde31a8549153ed38686d66
DolagySameh <dolagysameh20@gmail.com>
2023-09-13 16:45:08 -0700

    FOUSTH commit task

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+* main - test
+* Description:test function
+* @argc:first arg
+* @argv:second arg
+* Return:int
+*/
+int main(int argc, char *argv[])
+{
+	int (*ptr)(int, int), a, b;
+
+	if (argc != 4)
+        {
+		printf("Erorr\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	ptr = get_op_func(argv[2]);
+	if (!ptr)
+	{
+		printf("Erorr\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '/' && !b) || (argv[2][0] == '%' && !b))
+	{
+		printf("Erorr\n");
+		exit(100);
+	}
+	printf("%d\n", ptr(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+* op_add - sum of a and b
+* Description:sum of a and b
+* @a:first arg
+* @b:second arg
+* Return:int
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+* op_sub - sub of a and b
+* Description:sub of a and b
+* @a:first arg
+* @b:second arg
+* Return:int
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+* op_mul - mul of a and b
+* Description:mul of a and b
+* @a:first arg
+* @b:second arg
+* Return:int
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+* op_div - div of a and b
+* Description:div of a and b
+* @a:first arg
+* @b:second arg
+* Return:int
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+* op_mod - mod of a and b
+* Description:mod of a and b
+* @a:first arg
+* @b:second arg
+* Return:int
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
