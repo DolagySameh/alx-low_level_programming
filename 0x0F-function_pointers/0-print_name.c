e855322461870461775ecb44409aace5e0685aad
DolagySameh <dolagysameh20@gmail.com>
2023-09-13 12:49:25 -0700

    FIRST commit Task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+* print_name - prints a name
+* Description:prints a name
+* @name:first arg
+* @f:second arg
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
