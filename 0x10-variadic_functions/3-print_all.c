e91ebccbceb7b6518e33d8f51483529cb69a1b37
DolagySameh <dolagysameh20@gmail.com>
2023-09-14 15:37:58 -0700

    LAST commit task

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+* print_all - prints anything
+* Description:prints anything
+* @format:first arg
+* @...:second arg
+*/
+void print_all(const char * const format, ...)
+{
+
+}
