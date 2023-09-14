2a7f347d05003ddc3909e93d2b9bdc139079b888
DolagySameh <dolagysameh20@gmail.com>
2023-09-14 15:03:57 -0700

    THIRD commit Task

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (; i < n; i++)
 	{
 		v = va_arg(op, char *);
-		printf("%s", (v == NULL) ? "nill" : v);
+		printf("%s", (v == NULL) ? "(nil)" : v);
 		if (separator != NULL && i != n - 1)
 			printf("%s", separator);
 	}
