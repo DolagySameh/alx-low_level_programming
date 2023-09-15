b839f5c02e2cc381827e350a17ba1eeaa54f608a
DolagySameh <dolagysameh20@gmail.com>
2023-09-15 06:52:41 -0700

    FIRST commit task

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,13 @@ void print_all(const char * const format, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
+/**
+ * struct print - A new struct type defining our print
+ *
+ * @symbol: Our symbol representing the data type
+ * @print_func: a function pointer that prints
+ *             a data type according to the symbol
+*/
 typedef struct print
 {
 	char *symbol;
