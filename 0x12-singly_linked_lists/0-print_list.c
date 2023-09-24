e80c3b9e56d0ffcec748473be55303892d8f3141
DolagySameh <DolagySameh1@example.com>
2023-09-24 11:21:03 -0700

    print data of nodes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+* print_list - prints all the elements of a list_t list.
+* @h:first arg
+* Return:size_t
+*/
+size_t print_list(const list_t *h)
+{
+	const list_t *ptr = h;
+	int count = 0;
+
+	while (ptr != NULL)
+	{
+		if (ptr->str == NULL)
+		{
+			printf("[0] (nil)");
+			printf("\n");
+		}
+		else
+		{
+			printf("[%d] ", ptr->len);
+			printf("%s", ptr->str);
+			printf("\n");
+		}
+		ptr = ptr->next;
+		count++;
+	}
+	return (count);
+}
