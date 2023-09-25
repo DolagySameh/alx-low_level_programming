8075c873deea018c549096bcdd9d4caf7fd2abb5
DolagySameh <DolagySameh1@example.com>
2023-09-25 08:49:14 -0700

    free linked list2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,10 +7,9 @@
 */
 void free_listint2(listint_t **head)
 {
-	listint_t *ptr;;
+	listint_t *ptr = *head;
 
-	ptr = *head;
-	if (head == NULL)
+	if (*head == NULL)
 		return;
 	while (ptr != NULL)
 	{
