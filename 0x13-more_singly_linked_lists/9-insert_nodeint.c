ed960a40f5daba6d3993a55992d6faf2ba323f9e
DolagySameh <DolagySameh1@example.com>
2023-09-25 12:24:11 -0700

    insert in certain position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		ptr = ptr->next;
 		i++;
 	}
-	if (i = idx - 1)
+	if (i == idx - 1)
 	{
 		new->next = ptr->next;
 		ptr->next = new;
