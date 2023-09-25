acef2e6a21d7ce648710581c30a401e6b917c60c
DolagySameh <DolagySameh1@example.com>
2023-09-25 12:29:35 -0700

    insert in certain position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,10 +3,10 @@
 #include "lists.h"
 /**
 * insert_nodeint_at_index - inserts a new node at a given position
-* @head: pointer to first node
-* @idx: index to add new node
-* @n: data to be added in new node
-* Return:  the address of the new node, or NULL if it failed
+* @head:pointer to first node
+* @idx:index to add new node
+* @n:data to be added in new node
+* Return:the address of the new node, or NULL if it failed
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
