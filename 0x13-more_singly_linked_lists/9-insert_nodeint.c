a532456017c2079b50d4b01c079ed26eb8bb6b97
DolagySameh <DolagySameh1@example.com>
2023-09-25 12:20:18 -0700

    insert in certain position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = (listint_t *)malloc(sizeof(listint_t));
 	if (new == NULL || head == NULL)
 		return (NULL);
-	new ->n = n;
+	new->n = n;
 	if (idx == 0)
 	{
 		new->next = *head;
