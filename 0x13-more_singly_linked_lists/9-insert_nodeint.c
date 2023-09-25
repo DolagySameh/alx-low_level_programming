06c5db06b4dcfdf68245d3c6f720f8745a464303
DolagySameh <DolagySameh1@example.com>
2023-09-25 11:32:27 -0700

    add node at certain position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,6 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		(*head)->next = new;
 		new->next = *head;
+		return (new);
 	}
 	while (ptr && i < idx - 1)
 	{
