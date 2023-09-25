0c7e519de88d0282b0d2ee5aae302b0dc55664e4
DolagySameh <DolagySameh1@example.com>
2023-09-25 11:02:47 -0700

    return nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,7 +11,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *ptr = head;
 	unsigned int i = 0;
 
-	while (i < index)
+	while (ptr && i < index)
 	{
 		ptr = ptr->next;
 		i++;
