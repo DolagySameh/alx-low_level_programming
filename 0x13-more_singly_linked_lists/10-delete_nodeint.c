a74ca46fa83a3e39861b5102e995c8f4f125c2c8
DolagySameh <DolagySameh1@example.com>
2023-09-26 14:24:36 -0700

    delete node at certain position

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,8 +14,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *ptr2 = *head;
 	unsigned int i = 0;
 
-	if (*head == NULL)
-		return (-1);
 	if (index == 0)
 	{
 		*head = ptr->next;
@@ -24,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		ptr = NULL;
 		return (1);
 	}
-	while (ptr && ptr2 && i <= index)
+	while (ptr && ptr->next && ptr2 && i <= index)
 	{
 		if (i == index)
 		{
