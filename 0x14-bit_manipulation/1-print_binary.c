f806386e3e4006363ac3b7fd7dac06aee239d94c
DolagySameh <DolagySameh1@example.com>
2023-09-28 11:46:32 -0700

    convert number to binary without / or %

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
 * print_binary - prints the binary representation of a number
@@ -10,8 +9,8 @@ void print_binary(unsigned long int n)
 	{
 		if (n >> 1)
 			print_binary(n / 2);
-		putchar((n & 1) + '0');
+		_putchar((n & 1) + '0');
 	}
 	else
-		putchar('0');
+		_putchar('0');
 }
