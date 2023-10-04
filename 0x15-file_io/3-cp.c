c5a3c79fade2d7a967425b7c16994626edeed46e
DolagySameh <DolagySameh1@example.com>
2023-10-04 07:20:24 -0700

    append

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,27 +1,29 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
+
+#define MAXSIZE 1024
+
+
 /**
-* _exit11 - exit status
-* @error: first arg
-* @str: escond arg
-* @fd: thied arg
-* Return: int
+ * __exit - prints error messages and exits with exit number
+ *
+ * @error: either the exit number or file descriptor
+ * @str: name of either file_in or file_out
+ * @fd: file descriptor
+ *
+ * Return: 0 on success
 */
-int _exit11(int error, char *str, int fd)
+int __exit(int error, char *str, int fd)
 {
 	switch (error)
 	{
 		case 97:
-			dprintf(2, "Usage: cp file_from file_to\n");
+			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 			exit(error);
 		case 98:
-			dprintf(2, "Error: Can't read from file NAME_OF_THE_FILE %s\n", s);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
 			exit(error);
 		case 99:
-			dprintf(2, "Error: Can't write to NAME_OF_THE_FILE %s\n", s);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
 			exit(error);
 		case 100:
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
@@ -30,39 +32,55 @@ int _exit11(int error, char *str, int fd)
 			return (0);
 	}
 }
+
 /**
-* main - cp from file to another
-* @argc: first argument
-* @argv: second arg
-* Return: 0
+ * main - create a copy of file
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0 for success.
 */
 int main(int argc, char *argv[])
 {
-	int fpto, fpfrom, count1, count2, close1, close2;
-	char buffer[1024];
+	int file_in, file_out;
+	int read_stat, write_stat;
+	int close_in, close_out;
+	char buffer[MAXSIZE];
 
+	/*if arguments are not 3*/
 	if (argc != 3)
-		_exit11(97, NULL, 0);
-	fpto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fpto == -1)
-		_exit11(99, argv[2], 0);
-	fpfrom = open(argv[1], O_RDONLY);
-	if (fpfrom == -1)
-		_exit11(98, argv[1], 0);
-	count1 = read(fpfrom, buffer, 1024);
-	if (count1 == -1)
-		_exit11(98, argv[1], 0);
-	else
+		__exit(97, NULL, 0);
+
+	/*sets file descriptor for copy from file*/
+	file_in = open(argv[1], O_RDONLY);
+	if (file_in == -1)
+		__exit(98, argv[1], 0);
+
+	/*sets file descriptor for copy to file*/
+	file_out = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (file_out == -1)
+		__exit(99, argv[2], 0);
+
+	/*reads file_in as long as its not NULL*/
+	while ((read_stat = read(file_in, buffer, MAXSIZE)) != 0)
 	{
-		count2 = write(fpto, buffer, count1);
-		if (count2 == -1)
-			_exit11(99, argv[2], 0);
+		if (read_stat == -1)
+			__exit(98, argv[1], 0);
+
+		/*copy and write contents to file_out*/
+		write_stat = write(file_out, buffer, read_stat);
+		if (write_stat == -1)
+			__exit(99, argv[2], 0);
 	}
-	close1 = close(fpfrom);
-	if (close1 == -1)
-		_exit11(100, NULL, close1);
-	close2 = close(fpto);
-	if (close2 == -1)
-		_exit11(100, NULL, close2);
+
+	close_in = close(file_in); /*close file_in*/
+	if (close_in == -1)
+		__exit(100, NULL, file_in);
+
+	close_out = close(file_out); /*close file_out*/
+	if (close_out == -1)
+		__exit(100, NULL, file_out);
+
 	return (0);
 }
